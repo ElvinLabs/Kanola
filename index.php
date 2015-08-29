e8ff17a95e589a470005573c49f3dac1c803d659
Prasanna Rodrigo <rodrigo.rajitha@gmail.com>
2015-08-29 23:02:21 +0530

    new index
    copy content from tester to index

--- index.php
+++ index.php
@@ -5,36 +5,105 @@
 <title>KANOLA</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="description" content="" />
-<meta name="author" content="http://bootstraptaste.com" />
+<meta name="author" content="http://elvinlabs.com" />
 <!-- css -->
+    
+    
+    
 <link href="css/bootstrap.min.css" rel="stylesheet" />
-<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
-<link href="css/jcarousel.css" rel="stylesheet" />
-<link href="css/flexslider.css" rel="stylesheet" />
 <link href="css/style.css" rel="stylesheet" />
 <link rel="shortcut icon" href="fav.ico" />
-
-<!-- Theme skin -->
 <link href="skins/default.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+    
+    
+    <style>     
+        .logo{
+            height: 45px;
+            margin-top: 5px;
+            float: left;      
+        
+        }     
+        .container{
+            max-width: 900px;  
+        }            
+        .container .flexslider img{ 
+            max-width: 900px;
+            
+        }          
+        .navbar-collapse{
+            max-height: 60px;           
+        }     
+        .navbar .nav > li >a{     
+            font-size: 14px;
+            line-height: 15px 
+        }     
+        navbar-header .col-md-4 {
+           margin-left: 0px;
+           padding-left: 0px;
+       }    
+        @media (max-width: 767px) {
+             .navbar-header{
+                 margin-top:0px;
+             }
+        }    
+            #featured .flexslider {
+            
+                margin: 0px;
+                height: 360px;
+                overflow: hidden;
 
-<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-<!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+        }      
+    </style>
+
+
 
 </head>
 <body>
 <div id="wrapper">
 	<!-- start header -->
-	<?php
-        include "nav.html"
-    ?>
-	<!-- end header -->
-	<section id="featured">
+
+    <header>
+        <div class="navbar navbar-default navbar-static-top" >        
+            <div class="container" >   
+               <div class="navbar-header row" style="">                 
+                   <div class="col-md-4 col-sm-4 col-xs-4" style="padding-left:0px;">         
+                     <div style="" >                  
+                             <img class="logo" src="img/logo.png">   
+                     </div>             
+                    </div>             
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>                                     
+                </div>               
+                <div class="navbar-collapse collapse ">
+                  <ul class="nav navbar-nav">
+                        <li ><a href="index.php">Home</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle " data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false">Sectors<b class=" icon-angle-down"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="engineering.php"><b>Construction &amp; Engineering</b></a></li>
+                                <li><a href="auto.php"><b>Auto &amp; Heavy Machinary</b></a></li>
+                                <li><a href="textiles.php"><b>Textile</b></a></li>
+                            </ul>
+                        </li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
+                    </ul>                
+                </div>              
+            </div>       
+        </div>
+	</header>
+    
+    
+    <div class="container">   
+    	<section id="featured">
 	<!-- start slider -->
 <!--	<div class="container">-->
-		<div class="row" style="margin-bottom: 0px">
-			<div class="col-lg-12" style="margin-top: -50px">
+		  <div class="row" style="margin-bottom: 0px">
+			<div class="col-lg-12" style="">
             <!-- Slider -->
                 <div id="main-slider" class="flexslider">
                     <ul class="slides">
@@ -51,27 +120,18 @@
                 </div>
             <!-- end slider -->
 			</div>
-		</div>
-<!--	</div>	-->
-	
-	
-
-	</section>
-	<section class="callaction">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-				<div class="big-cta">
-					<div class="cta-text" >
-                        <h2 style="font-size:50px">Welcome to <span>Kanola</span></h2>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	</section>
-	<section id="content" style="padding:0px">
-	<div class="container" style="margin-top:20px">
+		  </div>
+<!--	</div>	-->	
+	   </section>      
+      </div>
+    
+    
+    
+    
+    
+    
+    
+    <div class="container" style="margin-top:20px">
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="row">
@@ -97,14 +157,92 @@
 			</div>
 		</div>
         </div>
-	<?php
-        include "footer.html"
-    ?>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <footer style="" id="contact">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="widget">
+					<h5 class="widgetheading">Get in touch with us</h5>
+					<address>
+					<strong>Kanola Construction &amp; Engineering</strong><br>
+					<strong>Kanola Auto &amp; Heavy Machinery</strong><br>
+					 Ihala Kalankuttiya , Kalankuttiya,<br>Sri Lanka.</address>
+					<p>
+						
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="widget">
+					<h5 class="widgetheading">Pages</h5>
+					<ul class="link-list">
+						<li><a href="engineering.php">Construction &amp; Engineering</a></li>
+						<li><a href="auto.php">Auto &amp; Heavy Machinaru</a></li>
+						<li><a href="textiles.php">Textiles</a></li>
+						<li><a href="about.php">About Us</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="widget">
+					<ul class="list-unstyled">
+                        <li><i class="fa fa-phone fa-fw"></i>Office : +94 25 7200220</li>
+                        <li><i class="fa fa-phone fa-fw"></i>Mobile :+94 71 4270539 ,+94 71 9925125</li>
+                        <li><i class="fa fa-phone fa-fw"></i>Fax  : +94 25 2249616</li>
+                        <li><i class="fa fa-envelope-o fa-fw"></i>
+                            <a href="mailto:name@example.com">kanolathilak@gmail.com</a>
+                        </li>
+                    </ul>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<div id="sub-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="copyright">
+						<p>
+							<span>&copy; KANOLA KCE 2015 All right reserved. By </span><a href="http://elvinlabs.com" target="_blank">ElvinLabs</a>
+						</p>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	</footer>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+       
+    
 </div>
 <a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
-<!-- javascript
-    ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
+
+    
 <script src="js/jquery.js"></script>
 <script src="js/jquery.easing.1.3.js"></script>
 <script src="js/bootstrap.min.js"></script>
@@ -116,5 +254,6 @@
 <script src="js/jquery.flexslider.js"></script>
 <script src="js/animate.js"></script>
 <script src="js/custom.js"></script>
+
 </body>
 </html>
