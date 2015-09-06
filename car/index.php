3d9728f217246361d476726fe0cac273fa0b65a4
Dileepa Chandima <dilipchandima@gmail.com>
2015-09-06 08:30:01 +0530

    auto sale

---
+++ car/index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>KANOLA</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="http://bootstraptaste.com" />
+<!-- css -->
+<link href="../css/bootstrap.min.css" rel="stylesheet" />
+<link href="../css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="../css/jcarousel.css" rel="stylesheet" />
+<link href="../css/flexslider.css" rel="stylesheet" />
+<link href="../css/style.css" rel="stylesheet" />
+<link rel="../shortcut icon" href="fav.ico" />
+<link href="../skins/default.css" rel="stylesheet" />
+
+</head>
+<body>
+<div id="wrapper">
+
+	<!-- start header -->
+	<?php
+        include "../nav/nav.html"
+    ?>
+	<!-- end header -->
+<!--
+	<section id="inner-headline">
+	<div class="container">
+		<div class="row">
+			<ul class="breadcrumb">
+					<li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+					<li><a href="#">Sectors</a><i class="icon-angle-right"></i></li>
+					<li class="active">About us</li>
+				</ul>
+		</div>
+	</div>
+	</section>
+-->
+	<section id="content" class="about">
+	<div class="container">
+        <div class="row">
+        	<div class="col-lg-12 pagetop">
+    			<img src="../img/pagetop/auto.jpg">
+    		</div>
+			<div class="col-lg-12">
+				<div class="box-gray">
+                    <h3>Car and Vehicle</h3>                                
+				</div>
+                
+                
+                <div class="col-lg-6" id="auto">
+                        <div class="col-lg-4">
+                            <img style="border-radius:7px" src="img/1.jpg" width="100%">
+                        </div>
+                        <div class="col-lg-8">
+                            <p><b>Honda GP5 L Grade 2014</b><br>
+                            11,000 km<br>
+                            <strong>Rs 3,000,000</strong></p>
+                        </div>
+                </div>
+                
+                <div class="col-lg-6" id="auto">
+                        <div class="col-lg-4">
+                            <img style="border-radius:7px" src="img/1.jpg" width="100%">
+                        </div>
+                        <div class="col-lg-8">
+                            <p><b>Honda GP5 L Grade 2014</b><br>
+                            11,000 km<br>
+                            <strong>Rs 3,000,000</strong></p>
+                        </div>
+                </div>
+                
+                <div class="col-lg-6" id="auto">
+                        <div class="col-lg-4">
+                            <img style="border-radius:7px" src="img/1.jpg" width="100%">
+                        </div>
+                        <div class="col-lg-8">
+                            <p><b>Honda GP5 L Grade 2014</b><br>
+                            11,000 km<br>
+                            <strong>Rs 3,000,000</strong></p>
+                        </div>
+                </div>
+                
+                <div class="col-lg-6" id="auto">
+                        <div class="col-lg-4">
+                            <img style="border-radius:7px" src="img/1.jpg" width="100%">
+                        </div>
+                        <div class="col-lg-8">
+                            <p><b>Honda GP5 L Grade 2014</b><br>
+                            11,000 km<br>
+                            <strong>Rs 3,000,000</strong></p>
+                        </div>
+                </div>
+                
+                
+                
+			</div>
+		</div>
+        
+	</div>
+	</section>
+<!--	footer-->
+    <?php
+        include "../footer/footer.html"
+    ?>
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+ <script src="../js/jquery.js"></script>
+<script src="../js/jquery.easing.1.3.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/jquery.fancybox.pack.js"></script>
+<script src="../js/jquery.fancybox-media.js"></script>
+<script src="../js/google-code-prettify/prettify.js"></script>
+<script src="../js/portfolio/jquery.quicksand.js"></script>
+<script src="../js/portfolio/setting.js"></script>
+<script src="../js/jquery.flexslider.js"></script>
+<script src="../js/animate.js"></script>
+<script src="../js/custom.js"></script>
+</body>
+</html>
